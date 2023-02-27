81fbe3f4688b5b7bf32ead587e4e429a5a49084b
drachins <djfrachinksky@gmail.com>
2023-02-26 22:03:22 -0700

    Editing README file

--- build/CMakeFiles/SnakeGame.dir/cmake_clean.cmake
+++ build/CMakeFiles/SnakeGame.dir/cmake_clean.cmake
@@ -1,18 +1,11 @@
 file(REMOVE_RECURSE
   "CMakeFiles/SnakeGame.dir/src/ai_snake.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/ai_snake.cpp.o.d"
   "CMakeFiles/SnakeGame.dir/src/controller.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/controller.cpp.o.d"
   "CMakeFiles/SnakeGame.dir/src/game.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/game.cpp.o.d"
   "CMakeFiles/SnakeGame.dir/src/main.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/main.cpp.o.d"
   "CMakeFiles/SnakeGame.dir/src/obstacle.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/obstacle.cpp.o.d"
   "CMakeFiles/SnakeGame.dir/src/renderer.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/renderer.cpp.o.d"
   "CMakeFiles/SnakeGame.dir/src/snake.cpp.o"
-  "CMakeFiles/SnakeGame.dir/src/snake.cpp.o.d"
   "SnakeGame"
   "SnakeGame.pdb"
 )
